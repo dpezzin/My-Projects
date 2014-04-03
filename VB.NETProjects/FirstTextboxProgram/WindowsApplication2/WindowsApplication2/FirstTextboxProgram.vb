2f0fc7d1760a9c3552941e9e15bf9a712b1938a7
Dwight Shrute <danpezzin@gmail.com>
2014-04-03 14:40:11 -0400

    first commit

---
+++ VB.NETProjects/FirstTextboxProgram/WindowsApplication2/WindowsApplication2/FirstTextboxProgram.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmMain
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdOK.Click
+
+        'A messagebox will be showed with the text from the textbox
+        MessageBox.Show(txtName.Text)
+
+    End Sub
+
+End Class
---
+++ VB.NETProjects/FirstTextboxProgram/WindowsApplication2/WindowsApplication2/frmMain.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdOK = New System.Windows.Forms.Button()
+        Me.LabName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'cmdOK
+        '
+        Me.cmdOK.Location = New System.Drawing.Point(108, 59)
+        Me.cmdOK.Name = "cmdOK"
+        Me.cmdOK.Size = New System.Drawing.Size(75, 23)
+        Me.cmdOK.TabIndex = 0
+        Me.cmdOK.Text = "OK"
+        Me.cmdOK.UseVisualStyleBackColor = True
+        '
+        'LabName
+        '
+        Me.LabName.AutoSize = True
+        Me.LabName.Location = New System.Drawing.Point(157, 151)
+        Me.LabName.Name = "LabName"
+        Me.LabName.Size = New System.Drawing.Size(250, 17)
+        Me.LabName.TabIndex = 1
+        Me.LabName.Text = "Please enter your name and press OK"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(225, 253)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 22)
+        Me.txtName.TabIndex = 2
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(528, 441)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.LabName)
+        Me.Controls.Add(Me.cmdOK)
+        Me.Name = "frmMain"
+        Me.Text = "My First Program"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdOK As System.Windows.Forms.Button
+    Friend WithEvents LabName As System.Windows.Forms.Label
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+
+End Class
